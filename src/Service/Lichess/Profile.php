e15705b5ed9aac4ede32fac957a01c9d36f54c4f
Pepe Doval <pepellou@gmail.com>
2019-07-01 12:13:42 -0500

    Service to access lichess profile (WIP)

--- src/Service/Lichess/Profile.php
+++ src/Service/Lichess/Profile.php
@@ -9,12 +9,12 @@ class Profile
     public function __construct($data)
     {
         $this->country = $data->country;
-        $this->location = $data->location;
-        $this->bio = $data->bio;
-        $this->firstName = $data->firstName;
-        $this->lastName = $data->lastName;
-        $this->fideRating = $data->fideRating;
-        $this->links = $data->links;
+        //$this->location = $data->location;
+        //$this->bio = $data->bio;
+        //$this->firstName = $data->firstName;
+        //$this->lastName = $data->lastName;
+        //$this->fideRating = $data->fideRating;
+        //$this->links = $data->links;
     }
 
 }
